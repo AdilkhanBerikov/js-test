f3c8c3a20557be33b252d6f4718792798c91d52c
Adilkhan <fant333777@gmail.com>
2025-11-23 15:46:09 +0500

    timer.js, timer.css

---
+++ src/timer.js
@@ -0,0 +1,61 @@
+import './timer.css'
+
+const timer = document.querySelector('.timer__display');
+const input = document.querySelector('.timer__input');
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+
+let timerId = null;
+let isRunning = false;
+
+start.addEventListener('click', () => {
+    if (!isRunning) {
+        startTimer();
+    }
+})
+
+stop.addEventListener('click',() => {
+    if (isRunning) {
+        stopTimer();
+    }
+})
+
+function startTimer() {
+    let seconds = Number(input.value);
+
+    if (isNaN(seconds) || seconds < 0) {
+        input.textContent = 'Введите число';
+        return false;
+    }
+
+    isRunning = true;
+
+    timerId = setInterval(() => {
+        seconds--;
+        timer.textContent = seconds;
+
+        if (seconds <= 0) {
+            clearInterval(timerId);
+            timerId = null;
+            isRunning = false;
+            timer.textContent = '0000';
+
+            timer.classList.add('timer__finished');
+            document.querySelector('#message').innerHTML = `
+                <p class="timer__finished">Время вышло!<p>
+            `;
+
+            setTimeout(() => {
+                timer.classList.remove('timer__finished');
+                document.querySelector('#message').innerHTML = '';
+            }, 3000)
+        }
+    }, 1000)
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+    timerId = null;
+    isRunning = false;
+    timer.textContent = '0000';
+}
