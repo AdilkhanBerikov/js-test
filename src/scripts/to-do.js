6b977ef33e0d840704d321904df9487e1f40904e
Adilkhan <fant333777@gmail.com>
2025-12-01 17:57:21 +0500

    fend-10

---
+++ src/scripts/to-do.js
@@ -0,0 +1,124 @@
+const form = document.querySelector('.to-do__form');
+const input = document.querySelector('.to-do__input');
+const button = document.querySelector('.to-do__button');
+const list = document.querySelector('.to-do__list');
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    console.log(tasks)
+}
+
+function renderTasks() {
+    tasks.forEach(task => {
+        let taskItem = `
+        <li class="to-do__item ${task.done ? "done" : ""}" id="${task.id}">
+            <div class="to-do__task">
+                <label>
+                    <input class="task__input" type="checkbox">
+                    <span>${task.text}</span>
+                </label>
+                <div>
+                    <button class="task__done" type="button">
+                        Done
+                    </button>
+                    <button class="task__delete" type="button">
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </li>`;
+
+        list.insertAdjacentHTML('beforeend', taskItem);
+    })
+}
+
+renderTasks();
+
+function addTask(event) {
+    event.preventDefault();
+
+    const task = input.value;
+
+    const taskInfo = {
+        id: Date.now(),
+        text: task,
+        done: false,
+    }
+
+    tasks.push(taskInfo);
+
+    let taskItem = `
+        <li class="to-do__item" id="${taskInfo.id}">
+            <div class="to-do__task">
+                <label>
+                    <input class="task__input" type="checkbox">
+                    <span>${taskInfo.text}</span>
+                </label>
+                <div>
+                    <button class="task__done" type="button">
+                        Done
+                    </button>
+                    <button class="task__delete" type="button">
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </li>`;
+
+    list.insertAdjacentHTML('beforeend', taskItem);
+
+    saveLocalStorage();
+}
+
+function removeTask(event) {
+
+    if (event.target.classList.contains('task__delete')) {
+        const targetItem = event.target.closest('.to-do__item');
+
+        const taskID = Number(targetItem.id);
+
+        tasks = tasks.filter(function (task) {
+            if (task.id === taskID) {
+                return false;
+            } else {
+                return true;
+            }
+        })
+
+        targetItem.remove();
+    }
+    saveLocalStorage();
+}
+
+function doneTask(event) {
+
+    const taskInput = document.querySelector('.task__input');
+
+    if (event.target.classList.contains('task__done')) {
+        const targetItem = event.target.closest('.to-do__item');
+
+        const taskID = Number(targetItem.id);
+
+        const task = tasks.find(function (task) {
+            if (task.id === taskID) {
+                return true;
+            }
+        })
+
+        task.done = !task.done
+
+        targetItem.classList.toggle("done");
+    }
+
+    saveLocalStorage();
+}
+
+function saveLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+form.addEventListener('submit', addTask);
+list.addEventListener('click', removeTask);
+list.addEventListener('click', doneTask);
